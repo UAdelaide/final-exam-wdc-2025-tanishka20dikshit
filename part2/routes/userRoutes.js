913e1e5c1f8355b89998c970bf1bc2be49e6e954
Tanishka Dikshit <dikshittanishka@gmail.com>
2025-06-20 13:42:48 +0000

    [automatic commit] 2025-06-20 13:42:48 UTC ;; communications error to 208.67.222.222#53: timed out 103.232.160.70

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -74,7 +74,9 @@ router.post('/login', async (req, res) => {
 
 router.get('/api/dogs', async (req, res) => {
   try {
-    const [dogs] = await db.execute(`SELECT dog_id, name AS dog_name, size, photo FROM Dogs`);
+    const [dogs] = await db.execute(`SELECT Dogs.dog_id, Dogs.name AS dog_name, Dogs.size, Dogs.photo, Users.username AS owner_username
+      FROM Dogs
+      JOIN Users ON Dogs.owner_id = Users.user_id`);
     res.json(dogs);
   } catch (err) {
     console.error(err);
