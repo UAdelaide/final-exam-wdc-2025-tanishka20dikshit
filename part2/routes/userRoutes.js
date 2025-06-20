d569094421ac810b971907828b5cc40d19c3b4b2
Tanishka Dikshit <dikshittanishka@gmail.com>
2025-06-20 10:46:31 +0000

    [automatic commit] 2025-06-20 10:46:31 UTC 103.232.160.70

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -43,7 +43,7 @@ router.get('/dogs', async (req, res) => {
 
   try {
     const ownerID = req.session.user.user_id;
-    const [rows] = await db.query('SELECT dog_id, name FROM Dogs WHERE ownerID = ?', [ownerID]);
+    const [rows] = await db.query('SELECT dog_id, name FROM Dogs WHERE owner_id = ?', [ownerID]);
     res.json(rows);
   }
 });
