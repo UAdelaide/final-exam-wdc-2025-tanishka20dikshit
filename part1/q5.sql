955c47700f44a08693172daab848c1b18ed88b5a
Tanishka Dikshit <dikshittanishka@gmail.com>
2025-06-20 00:42:40 +0000

    [automatic commit] 2025-06-20 00:42:40 UTC 103.232.160.70

--- part1/q5.sql
+++ part1/q5.sql
@@ -1,6 +1,6 @@
 INSERT INTO Users (username, email, password_hash, role)
-VALUES (('alice123','alice@example.com','hashed123','owner'),
-        ('bobwalker','bob@example.com','hashed456','walker'),
-        ('carol123','carol@example.com','hashed789','owner'),
-        ('jonh123','johndoe@example.com','hashed101','owner'),
+VALUES ('alice123','alice@example.com','hashed123','owner'),
+       ('bobwalker','bob@example.com','hashed456','walker'),
+       ('carol123','carol@example.com','hashed789','owner'),
+       ('jonh123','johndoe@example.com','hashed101','owner'),
         ('amanda123','amanda@example.com','hashed122','walker');
