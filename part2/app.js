42dde429403be9310474deeac334e2cb4cb06404
Tanishka Dikshit <dikshittanishka@gmail.com>
2025-06-20 11:02:40 +0000

    [automatic commit] 2025-06-20 11:02:40 UTC ;; communications error to 208.67.222.222#53: timed out 103.232.160.70

--- part2/app.js
+++ part2/app.js
@@ -29,7 +29,7 @@ app.use('/api/users', userRoutes);
 // module.exports = app;
 
 // const port = 3000;
-// app.listen(port, () => {
-//   console.log(`Server running at http://localhost:${port}`);
-// });
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
 
