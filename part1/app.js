4fde31b1ec4d90685457bf78a1312aeb3c40190d
Tanishka Dikshit <dikshittanishka@gmail.com>
2025-06-20 01:23:49 +0000

    [automatic commit] 2025-06-20 01:23:49 UTC 103.232.160.70

--- part1/app.js
+++ part1/app.js
@@ -34,16 +34,15 @@ let db;
       database: 'DogWalkService'
     });
 
-    // Create a table if it doesn't exist
-    await db.execute(`
-      CREATE TABLE IF NOT EXISTS books (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        title VARCHAR(255),
-        author VARCHAR(255)
-      )
-    `);
+    // // Create a table if it doesn't exist
+    // await db.execute(`
+    //   CREATE TABLE IF NOT EXISTS books (
+    //     id INT AUTO_INCREMENT PRIMARY KEY,
+    //     title VARCHAR(255),
+    //     author VARCHAR(255)
+    //   )
+    // `);
 
-    // Insert data if table is empty
     const [rows] = await db.execute('SELECT COUNT(*) AS count FROM books');
     if (rows[0].count === 0) {
       await db.execute(`
