4146b0ca40645046966adcc35901f39bc37af0c1
Tanishka Dikshit <dikshittanishka@gmail.com>
2025-06-20 07:57:09 +0000

    [automatic commit] 2025-06-20 07:57:09 UTC 103.232.160.70

--- part2/app.js
+++ part2/app.js
@@ -8,6 +8,8 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
 app.use(express.static(path.join(__dirname, '/public')));
 
 // Routes
