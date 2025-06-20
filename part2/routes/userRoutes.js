a716fe09c4c42d6cb6ed260a7a384006e77ed186
Tanishka Dikshit <dikshittanishka@gmail.com>
2025-06-20 10:42:01 +0000

    [automatic commit] 2025-06-20 10:42:01 UTC 103.232.160.70

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -38,7 +38,7 @@ router.get('/me', (req, res) => {
 
 router.get('/dogs', async (req, res)=> {
   if(!req.session.user || req.session.user.role !== 'owner') {
-    
+    return res.status(401).json({})
   }
 }
 )
