af8480476a2e7802bb3d5697afae732831eb6d54
Tanishka Dikshit <dikshittanishka@gmail.com>
2025-06-20 00:49:48 +0000

    [automatic commit] 2025-06-20 00:49:48 UTC 103.232.160.70

--- part1/q5.sql
+++ part1/q5.sql
@@ -12,4 +12,4 @@ VALUES ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'
        ((SELECT user_id FROM Users WHERE username = 'john123'), 'coco', 'large'),
        ((SELECT user_id FROM Users WHERE username = 'john123'), 'Maxine', 'medium'),
 
-INSERT INTO WalkRequests (dog_id, )
+INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
