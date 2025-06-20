1a35bf6fbf505caf1a81272b3d09739a135a084d
Tanishka Dikshit <dikshittanishka@gmail.com>
2025-06-20 02:23:01 +0000

    [automatic commit] 2025-06-20 02:23:01 UTC ;; communications error to 208.67.222.222#53: timed out 103.232.160.70

--- part1/app.js
+++ part1/app.js
@@ -68,7 +68,8 @@ app.get('/api/walkrequests/open', async (req, res) => {
 app.get('/api/walkers/summary ', async (req, res) => {
   try {
     const [summary] = await db.execute(`SELECT user.username AS walker_username,
-      COUNT (rating.)
+      COUNT (rating.rating_id) AS total_ratings,
+      
         `);
     res.json(summary);
   } catch (err) {
