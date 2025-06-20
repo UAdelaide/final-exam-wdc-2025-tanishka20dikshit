d19d749e01a6bb7de6ee72d54ad0b5efd0a548b6
Tanishka Dikshit <dikshittanishka@gmail.com>
2025-06-20 09:02:07 +0000

    [automatic commit] 2025-06-20 09:02:06 UTC 103.232.160.70

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -58,11 +58,12 @@ router.post('/login', async (req, res) => {
 });
 
 router.post('/logout', async (req, res) => {
-  res.session.destroy(err => {
+  res.session.destroy((err) => {
     if (err) {
       return res.send(500).json({ error: 'Failed to log out'});
     }
-    res.clearCookie()
+    res.clearCookie('connect.sid');
+    res.json({})
   })
 
 
