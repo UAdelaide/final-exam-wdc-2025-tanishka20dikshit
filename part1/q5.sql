6f1cddb2f2f823ffbdddb1c079a07e4f2bd468dd
Tanishka Dikshit <dikshittanishka@gmail.com>
2025-06-20 00:54:55 +0000

    [automatic commit] 2025-06-20 00:54:55 UTC 103.232.160.70

--- part1/q5.sql
+++ part1/q5.sql
@@ -5,16 +5,16 @@ VALUES ('alice123','alice@example.com','hashed123','owner'),
        ('jonh123','johndoe@example.com','hashed101','owner'),
        ('amanda123','amanda@example.com','hashed122','walker');
 
-INSERT INTO Dog (owner_id, name, size)
+INSERT INTO Dogs (owner_id, name, size)
 VALUES ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'),
        ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Bella', 'small'),
        ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Bruno', 'medium'),
        ((SELECT user_id FROM Users WHERE username = 'john123'), 'coco', 'large'),
-       ((SELECT user_id FROM Users WHERE username = 'john123'), 'Maxine', 'medium'),
+       ((SELECT user_id FROM Users WHERE username = 'john123'), 'Maxine', 'medium');
 
 INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status)
 VALUES ((SELECT dog_id FROM Dogs WHERE name = 'Max'),'2025-06-10 08:00:00',30,'Parklands','open'),
        ((SELECT dog_id FROM Dogs WHERE name = 'Bella'),'06-10 09:30:00',45,'Beachside Ave','accepted'),
        ((SELECT dog_id FROM Dogs WHERE name = 'Bruno'),'11-10 10:30:00',50,'Grant Ave','accepted'),
        ((SELECT dog_id FROM Dogs WHERE name = 'coco'),'06-10 09:35:00',45,'glenelg','open'),
-       ((SELECT dog_id FROM Dogs WHERE name = 'Maxine'),'09-10 09:30:00',60,'Walkerville','open'),
+       ((SELECT dog_id FROM Dogs WHERE name = 'Maxine'),'09-10 09:30:00',60,'Walkerville','open');
