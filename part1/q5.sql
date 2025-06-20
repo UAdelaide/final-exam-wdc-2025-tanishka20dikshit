0b4172647b705ae73098cff38e4c31503cde6db3
Tanishka Dikshit <dikshittanishka@gmail.com>
2025-06-20 00:40:38 +0000

    [automatic commit] 2025-06-20 00:40:38 UTC 103.232.160.70

--- part1/q5.sql
+++ part1/q5.sql
@@ -2,4 +2,4 @@ INSERT INTO Users (username, email, password_hash, role)
 VALUES (('alice123','alice@example.com','hashed123','owner'),
         ('bobwalker','bob@example.com','hashed456','walker'),
         ('carol123','carol@example.com','hashed789','owner'),
-        ('jonh123'))
+        ('jonh123','johndoe@example.com',hashed))
