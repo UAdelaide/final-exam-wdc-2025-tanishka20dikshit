8358248208373767b94d436759208416d4f10498
Tanishka Dikshit <dikshittanishka@gmail.com>
2025-06-20 02:34:23 +0000

    [automatic commit] 2025-06-20 02:34:23 UTC 103.232.160.70

--- part1/app.js
+++ part1/app.js
@@ -65,7 +65,7 @@ app.get('/api/walkrequests/open', async (req, res) => {
   }
 });
 
-app.get('/api/walkers/summary ', async (req, res) => {
+app.get('/api/walkers/summary', async (req, res) => {
   try {
     const [summary] = await db.execute(`SELECT user.username AS walker_username,
       COUNT(r.rating_id) AS total_ratings,
