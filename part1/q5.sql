80be7acc2ce63a99e79447a15cdab8666ac422ed
Tanishka Dikshit <dikshittanishka@gmail.com>
2025-06-20 00:47:25 +0000

    [automatic commit] 2025-06-20 00:47:25 UTC 103.232.160.70

--- part1/q5.sql
+++ part1/q5.sql
@@ -7,4 +7,5 @@ VALUES ('alice123','alice@example.com','hashed123','owner'),
 
 INSERT INTO Dog (owner_id, name, size)
 VALUES ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'),
-       ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Max', 'medium'),
+       ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Bella', 'small'),
+       ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'),
