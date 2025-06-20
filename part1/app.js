d15b4dc0f8b70a11aed537c6ecbbe2637c03f9af
Tanishka Dikshit <dikshittanishka@gmail.com>
2025-06-20 01:58:14 +0000

    [automatic commit] 2025-06-20 01:58:14 UTC 103.232.160.70

--- part1/app.js
+++ part1/app.js
@@ -41,7 +41,7 @@ app.get('/api/dogs', async (req, res) => {
   try {
     const [dogs] = await db.execute(`SELECT dog.name AS dog_name, dog.size AS dog_size, user.username AS owner_username
       FROM Dogs dog
-      JOIN Users user ON dog.owner
+      JOIN Users user ON dog.owner_id = user.user_id
       `);
     res.json(dogs);
   } catch (err) {
