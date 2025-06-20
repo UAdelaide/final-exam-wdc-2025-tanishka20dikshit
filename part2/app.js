8cf751c819360aa1649bd74ff35fc0edb16b3da6
Tanishka Dikshit <dikshittanishka@gmail.com>
2025-06-20 11:07:06 +0000

    [automatic commit] 2025-06-20 11:07:05 UTC 103.232.160.70

--- part2/app.js
+++ part2/app.js
@@ -25,11 +25,12 @@ const userRoutes = require('./routes/userRoutes');
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
 
-// Export the app instead of listening here
-// module.exports = app;
+
 
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
 
+// Export the app instead of listening here
+module.exports = app;
