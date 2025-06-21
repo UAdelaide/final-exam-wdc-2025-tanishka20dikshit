571c810355866610c80e905c87ac42a7a3a8f491
Tanishka Dikshit <dikshittanishka@gmail.com>
2025-06-21 01:55:00 +0000

    [automatic commit] 2025-06-21 01:55:00 UTC 103.232.160.70

--- part2/app.js
+++ part2/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const session = require('express-session');
 require('dotenv').config();
-var mysql = require('mysql2/promise');
+const mysql = require('mysql2/promise');
 
 const app = express();
 
