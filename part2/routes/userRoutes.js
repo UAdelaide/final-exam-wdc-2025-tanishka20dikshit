4ec871486f71cf1895c9da038e8360c23f53363b
Tanishka Dikshit <dikshittanishka@gmail.com>
2025-06-21 05:00:17 +0000

    [automatic commit] 2025-06-21 05:00:17 UTC 103.232.160.70

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -72,18 +72,6 @@ router.post('/login', async (req, res) => {
   }
 });
 
-// router.get('/dogs', async (req, res) => {
-//   try {
-//     const [dogs] = await db.execute(`SELECT Dogs.dog_id, Dogs.name AS dog_name, Dogs.size, Dogs.photo, Users.username AS owner_username
-//       FROM Dogs
-//       JOIN Users ON Dogs.owner_id = Users.user_id`);
-//     res.json(dogs);
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).json({ error: 'Failed to fetch dogs' });
-//   }
-// });
-
 router.post('/logout', async (req, res) => {
   req.session.destroy((err) => {
     if (err) {
